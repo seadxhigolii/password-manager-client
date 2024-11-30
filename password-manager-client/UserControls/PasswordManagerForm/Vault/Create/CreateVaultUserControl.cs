389dce81df313cbfac0beeda0ab894b21da42041
Sead Xhigoli <sead.xhigoli1@gmail.com>
2024-11-30 13:15:14 +0100

    add vault type dropdown initialization

--- password-manager-client/UserControls/PasswordManagerForm/Vault/Create/CreateVaultUserControl.cs
+++ password-manager-client/UserControls/PasswordManagerForm/Vault/Create/CreateVaultUserControl.cs
@@ -15,13 +15,26 @@
             _createSecureNoteUserControl = createSecureNoteUserControl;
 
 
-            Controls.Add(_createSecureNoteUserControl);
             Controls.Add(_createVaultWebsiteUserControl);
+            Controls.Add(_createSecureNoteUserControl);
 
             _createSecureNoteUserControl.Visible = false;
             _createVaultWebsiteUserControl.Visible = false;
 
             vault_type_dropdown.SelectedIndexChanged += VaultTypeDropdown_SelectedIndexChanged;
+            vault_type_dropdown.SelectedItem = "Login";
+            InitializeDropdown();
+        }
+
+        private void InitializeDropdown()
+        {
+            _createVaultWebsiteUserControl.Visible = true;
+            _createSecureNoteUserControl.Visible = true;
+
+            _createSecureNoteUserControl.Location = new Point(
+                _createVaultWebsiteUserControl.Location.X,
+                _createVaultWebsiteUserControl.Location.Y + _createVaultWebsiteUserControl.Height + 20);
+
         }
 
         private void VaultTypeDropdown_SelectedIndexChanged(object sender, EventArgs e)
@@ -32,13 +45,38 @@
             {
                 _createVaultWebsiteUserControl.Visible = false;
                 _createSecureNoteUserControl.Visible = true;
+
+                int newY = GetBottomMostControlY() + 80;
+                _createSecureNoteUserControl.Location = new Point(_createSecureNoteUserControl.Location.X, newY);
             }
             else if (selectedValue == "Login")
             {
-                _createSecureNoteUserControl.Visible = false;
                 _createVaultWebsiteUserControl.Visible = true;
+                _createSecureNoteUserControl.Visible = true;
+
+                _createSecureNoteUserControl.Location = new Point(
+                    _createVaultWebsiteUserControl.Location.X,
+                    _createVaultWebsiteUserControl.Location.Y + _createVaultWebsiteUserControl.Height + 20);
             }
         }
 
+        private int GetBottomMostControlY()
+        {
+            int maxY = 0;
+
+            foreach (Control control in Controls)
+            {
+                if (control.Visible)
+                {
+                    int bottomY = control.Location.Y + control.Height;
+                    if (bottomY > maxY)
+                    {
+                        maxY = bottomY;
+                    }
+                }
+            }
+
+            return maxY;
+        }
     }
 }
